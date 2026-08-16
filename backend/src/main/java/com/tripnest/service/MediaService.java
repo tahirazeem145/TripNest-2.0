3d2aa5fd7f7778553b9aad69d104d337b6057c40
tahirazeem145 <tahirazeems145s@gmail.com>
2026-08-16 21:45:12 +0530

    fix: remove unused import HttpClientErrorException in MediaService.java

--- backend/src/main/java/com/tripnest/service/MediaService.java
+++ backend/src/main/java/com/tripnest/service/MediaService.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
